ecfe58f8e492d8faf867b4f3ca791a139707733f
Amith Liyanage <amith.liyanage.9@gmail.com>
2019-02-24 12:31:38 +0530

    after 11 th video

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -4,41 +4,21 @@ import { Component, OnInit } from '@angular/core';
   selector: '.app-test',
   template: `
     <h2>
-      Welcome {{name }}
+      Welcome !
     </h2>
 
-    <h2 [style.color]="'orange'">Style Binding</h2>
-    <input #myInput type="text">
-    <button (click)="logMessage(myInput.value)">Log</button>
+    <input [(ngModel)]="name" type="text">
+    {{name}}
   `,
-  styles: [` 
-    
-  `] 
+  styles: [] 
 })
 export class TestComponent implements OnInit {
 
-  public name = "Codevoluation";
-  public successClass = "text-success";
-  public hasError = false;
-  public isSpecial = true;
-
-  onClick(){
-    console.log("Welcome to Codevoluation");
-  }
-
-  public messageClasses = {
-    "text-success": !this.hasError,
-    "text-danger": !this.hasError,
-    "text-special": this.isSpecial
-  }
+  public name = "";
 
   constructor() { }
 
   ngOnInit() { 
-  }
-
-  greetUser(){
-    return "Hello " + this.name;
   }
 
   logMessage(value) {
