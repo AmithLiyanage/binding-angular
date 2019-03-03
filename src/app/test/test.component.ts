f0e32389d2b2b2ad4017b4302627e8f7331367f8
Amith Liyanage <amith.liyanage.9@gmail.com>
2019-03-03 15:59:39 +0530

    after 17 th video

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -4,37 +4,21 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <h2>{{ name}}</h2>
-    <h2>{{ name | lowercase }}</h2>
-    <h2>{{ name | uppercase }}</h2>
-    <h2>{{ message | titlecase }}</h2>
-    <h2>{{ name | slice:3:5 }}</h2>
-    <h2>{{ person | json }}</h2>
-
-    <h2>{{ 5.678 | number:'1.2-3' }}</h2>
-    <h2>{{ 5.678 | number:'3.4-5' }}</h2>
-    <h2>{{ 5.678 | number:'3.1-2' }}</h2>
-
-    <h2>{{ 0.25 | percent }}</h2>
-    <h2>{{ 0.25 | currency }}</h2>
-    <h2>{{ 0.25 | currency: 'GBP' }}</h2>
-    <h2>{{ 0.25 | currency: 'EUR' : 'code' }}</h2>
-
-    <h2>{{ date }}</h2>
-    <h2>{{ date | date:'short' }}</h2>
-    <h2>{{ date | date:'shortDate' }}</h2>
-    <h2>{{ date | date:'shortTime' }}</h2>
+    <h2>Employee List</h2>
+    <ul *ngFor="let employee of employees">
+      <li>{{employee.name}}</li>
+    </ul>
   `,
   styles: [] 
 })
 export class TestComponent implements OnInit {
 
-  public name = "Codevolution";
-  public message = "Welcome to codevolution";
-  public person = { 
-    "firstName": "john",
-    "lastName": "Doe"
-  }
+  public employees = [
+    {"id": 1, "name": "Andrew", "age": 30},
+    {"id": 2, "name": "Brandon", "age": 25},
+    {"id": 3, "name": "Christins", "age": 26},
+    {"id": 4, "name": "Elena", "age": 28}
+  ];   
 
   public date = new Date();
 
