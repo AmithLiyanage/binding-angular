e26ba92797b9eb98aac3b3745e241e77487592ce
Amith Liyanage <amith.liyanage.9@gmail.com>
2019-03-03 19:18:06 +0530

    fix employee detail problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { EmployeeService } from './employee.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
     EmployeeListComponent,
     EmployeeDetailComponent
   ],
@@ -20,7 +19,7 @@ import { EmployeeDetailComponent } from './employee-detail/employee-detail.compo
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/employee-detail/employee-detail.component.ts
+++ src/app/employee-detail/employee-detail.component.ts
@@ -1,29 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'employee-detail',
   template: `
   <h2>Employee Detail</h2>
+  <ul *ngFor="let employee of employees">
+    <li>{{employee.id}}. {{employee.name}} - {{employee.age}}
+    </li>
+  </ul>
   `,
   styles: [] 
 })
 export class EmployeeDetailComponent implements OnInit {
 
-  public employees = [
-    {"id": 1, "name": "Andrew", "age": 30},
-    {"id": 2, "name": "Brandon", "age": 25},
-    {"id": 3, "name": "Christins", "age": 26},
-    {"id": 4, "name": "Elena", "age": 28}
-  ];
+  public employees = [];
 
-  
-    // <ul *ngFor="let employee of employees">
-    //   <li>{{employee.1d}}. {{employee.name}} - {{employee.age}}</li>
-    // </ul>
-
-  constructor() { }
+  constructor(private _employeeService: EmployeeService) { }
 
   ngOnInit() {
+    this.employees = this._employeeService.getEmployees();
   }
 
 }
